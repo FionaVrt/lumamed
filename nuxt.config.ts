5133ccec68479df04e793bb0648adaa1cd942751
fiona Vr <fiovert1192@gmail.com>
2024-12-11 16:52:04 +0100

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true }, // Virgule ajoutée ici
+  modules: ["@nuxtjs/tailwindcss"]
+});
